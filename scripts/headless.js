77698e6a3fce1ca4eca9f9c101702f691d67ebbd
jefbecker <d736r27f@duck.com>
2024-01-10 22:51:52 -0300

    scripts moved to a separate folder

---
+++ scripts/headless.js
@@ -0,0 +1,3 @@
+// Script - How to Change Your Neocities Thumbnail from ARandomSite: https://arandomsite.neocities.org/writing/#custom-neocities-thumbnail //
+
+if (/headless/i.test(window.navigator.userAgent)) { window.location.replace("/thumbnail/"); }
